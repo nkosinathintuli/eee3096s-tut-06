78c11cc6183d33c4a787cf6a5469d9767e572d60
Nkosinathi Ntuli <51000737+nkosinathintuli@users.noreply.github.com>
2021-10-22 15:20:02 +0200

    Modified the Testbench to be 8bits wide and 32 entries deep.

--- RegMem_tb.v
+++ RegMem_tb.v
@@ -2,8 +2,8 @@
 
 module tb_reg_mem;
 
-    parameter DATA_WIDTH = 4; //4 bit wide data
-    parameter ADDR_BITS = 3; //8 Addresses
+    parameter DATA_WIDTH = 8; //8 bit wide data
+    parameter ADDR_BITS = 5; //32 Addresses
 
     reg [ADDR_BITS-1:0] addr;
     reg [DATA_WIDTH-1:0] data_in;
@@ -14,16 +14,16 @@ module tb_reg_mem;
     reg_mem #(DATA_WIDTH,ADDR_BITS) RM (addr, data_in, wen, clk, data_out);
 
     initial begin
-        /* For use in EDAPlayground
+        //* For use in EDAPlayground
         $dumpfile("dump.vcd");
         $dumpvars(1, tb_reg_mem);
-        */
+        //*/
         
         clk = 0;
         wen = 1;
        
         //Write 10-17 to addresses 0-7 
-        for(integer i=10;i<18;i=i+1) 
+        for(int i=10;i<18;i=i+1) 
         begin
             data_in = i; 
             addr = (i+2);
@@ -33,7 +33,7 @@ module tb_reg_mem;
         wen =0;
         #1;
         //Read 10-17 from addresses 0-7 
-        for(integer i=10;i<18;i=i+1) 
+        for(int i=10;i<18;i=i+1) 
         begin
             data_in = i; 
             addr = (i+2);
